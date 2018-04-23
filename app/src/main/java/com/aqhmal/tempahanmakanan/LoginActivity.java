7419a7db90d7069ef432ef024b4b1db492449f39
Aqhmal Hafizi <aqhmal@geekbyte.net>
2018-04-23 23:21:09 +0800

    added gradient to login

--- app/src/main/java/com/aqhmal/tempahanmakanan/LoginActivity.java
+++ app/src/main/java/com/aqhmal/tempahanmakanan/LoginActivity.java
@@ -2,9 +2,11 @@ package com.aqhmal.tempahanmakanan;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -19,6 +21,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText username, password;
     Button loginBtn;
     SweetAlertDialog alert;
+    private ConstraintLayout constraintLayout;
+    private AnimationDrawable animationDrawable;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -26,6 +30,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide(); // Hide action bar
         setContentView(R.layout.activity_login);
+        // Get layout view
+        constraintLayout = findViewById(R.id.constraintLayout);
+        // initializing animation drawable by getting background from constraint layout
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        // setting enter fade animation duration to 5 seconds
+        animationDrawable.setEnterFadeDuration(5000);
+        // setting exit fade animation duration to 2 seconds
+        animationDrawable.setExitFadeDuration(2000);
         // Variables initialization
         username = findViewById(R.id.unameInput);
         password = findViewById(R.id.passInput);
@@ -85,5 +97,23 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            // start the animation
+            animationDrawable.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            // stop the animation
+            animationDrawable.stop();
+        }
     }
 }
