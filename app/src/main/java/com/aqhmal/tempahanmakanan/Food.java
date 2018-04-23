273ae777fde56bafe63c82ed92b0842bb57f5f5f
Aqhmal Hafizi <aqhmal@geekbyte.net>
2018-04-23 20:40:38 +0800

    Added Food model class

---
+++ app/src/main/java/com/aqhmal/tempahanmakanan/Food.java
@@ -0,0 +1,27 @@
+package com.aqhmal.tempahanmakanan;
+
+public class Food {
+    public int foodImage;
+    public String foodName;
+
+    public Food(String foodName, int foodImage) {
+        this.foodName =  foodName;
+        this.foodImage = foodImage;
+    }
+
+    public int getFoodImage() {
+        return foodImage;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodImage(int foodImage) {
+        this.foodImage = foodImage;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+}
