8ca19ee76ee63852f1b625b237072ea94a0c753b
Aqhmal Hafizi <aqhmal@geekbyte.net>
2018-04-23 12:35:23 +0800

    Updated login

--- app/src/main/java/com/aqhmal/tempahanmakanan/LoginActivity.java
+++ app/src/main/java/com/aqhmal/tempahanmakanan/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.aqhmal.tempahanmakanan;
 
-import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Objects;
+
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class LoginActivity extends AppCompatActivity {
@@ -23,11 +28,33 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginBtn);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
-                alert = new SweetAlertDialog(LoginActivity.this);
-                alert.setTitle("Blabla");
-                alert.show();
+                try {
+                    String uname = username.getText().toString();
+                    String pass = password.getText().toString();
+
+                    if(TextUtils.isEmpty((uname))) {
+                        throw new Exception("Username cannot be empty");
+                    }
+                    if(TextUtils.isEmpty(pass)) {
+                        throw new Exception("Password cannot be empty.");
+                    }
+                    if(!Objects.equals(uname, "root") || !Objects.equals(pass, "root")) {
+                        throw new Exception("Wrong username or password");
+                    } else {
+                        alert = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.SUCCESS_TYPE);
+                        alert.setTitleText("Good job!");
+                        alert.setContentText("You clicked the button!");
+                        alert.show();
+                    }
+                } catch(Exception e) {
+                    alert = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.WARNING_TYPE);
+                    alert.setTitleText("Invalid Input");
+                    alert.setContentText(e.getMessage() + "!");
+                    alert.show();
+                }
             }
         });
     }
