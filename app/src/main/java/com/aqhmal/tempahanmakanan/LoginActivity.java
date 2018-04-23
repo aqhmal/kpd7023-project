d7a2b26593ad543c33f538fc60d2a17ee7c5ad5c
Aqhmal Hafizi <aqhmal@geekbyte.net>
2018-04-23 15:51:49 +0800

    Added main activity and updated login

--- app/src/main/java/com/aqhmal/tempahanmakanan/LoginActivity.java
+++ app/src/main/java/com/aqhmal/tempahanmakanan/LoginActivity.java
@@ -1,9 +1,10 @@
 package com.aqhmal.tempahanmakanan;
 
+import android.graphics.Color;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -21,11 +22,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
         username = findViewById(R.id.unameInput);
         password = findViewById(R.id.passInput);
         loginBtn = findViewById(R.id.loginBtn);
+
+        final int[] attempt = {0};
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -39,10 +43,18 @@ public class LoginActivity extends AppCompatActivity {
                         throw new Exception("Username cannot be empty");
                     }
                     if(TextUtils.isEmpty(pass)) {
-                        throw new Exception("Password cannot be empty.");
+                        throw new Exception("Password cannot be empty");
                     }
                     if(!Objects.equals(uname, "root") || !Objects.equals(pass, "root")) {
-                        throw new Exception("Wrong username or password");
+                        // Increment attempt
+                        ++attempt[0];
+                        // Disable login button if attempt is 3
+                        if(attempt[0] >= 3) {
+                            loginBtn.setEnabled(false);
+                            throw new Exception("Login has been disabled");
+                        } else {
+                            throw new Exception("Invalid credential");
+                        }
                     } else {
                         alert = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.SUCCESS_TYPE);
                         alert.setTitleText("Good job!");
@@ -50,10 +62,12 @@ public class LoginActivity extends AppCompatActivity {
                         alert.show();
                     }
                 } catch(Exception e) {
-                    alert = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.WARNING_TYPE);
-                    alert.setTitleText("Invalid Input");
+                    alert = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.ERROR_TYPE);
+                    alert.setTitleText("Error");
                     alert.setContentText(e.getMessage() + "!");
+                    alert.setCancelable(false);
                     alert.show();
+                    alert.findViewById(R.id.confirm_button).setBackgroundColor(Color.parseColor("#fc5046"));
                 }
             }
         });
